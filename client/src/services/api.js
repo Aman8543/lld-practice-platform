8a6c09b12a403b1378ae40f2184a5eaf582767d8
*Aman <*dhanushag111@gmail.com*>
2026-09-25 21:41:45 +0530

    axios3

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lld-practice-platform-s5uc.onrender.com"||"http://localhost:5000/api"
+  baseURL: "https://lld-practice-platform-s5uc.onrender.com"
 });
 
 export const getProblems = () =>
